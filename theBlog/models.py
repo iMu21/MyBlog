4044d11c44481591e4e4125f7712cf291c26874c
Md Imran Khan <ik21.cse14@gmail.com>
2021-10-26 09:42:35 +0600

    third week update

--- theBlog/models.py
+++ theBlog/models.py
@@ -5,21 +5,20 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy
 from .managers import CustomUserManager
 from django.contrib.auth import get_user_model
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(ugettext_lazy('email address'), unique=True)
+    date_of_birth = models.DateField(blank=False,null=False)
+    phone = PhoneNumberField(null=True, blank=True, unique=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username','password','date_of_birth']
 
     objects = CustomUserManager()
-
-    spouse_name = models.CharField(blank=True,max_length=100)
-    date_of_birth = models.DateField(blank=True,null=True)
-
+    
     def __str__(self):
         return str(self.username)+" | "+str(self.email)
 
